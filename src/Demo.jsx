18b0e4b4be0ff151e4bc60e81990d1e75b621463
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-02-20 00:22:07 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,18 +1,38 @@
 import { useState } from "react";
 
 const Demo = () => {
+  const [page, setPage] = useState(1);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    role: "",
+    terms: false,
+  });
 
-  const [page,setPage]=useState(1)
-  const [formData,setFormData]=useState({
-    name:"",
-    email:"",
-    role:"",
-terms:false
+  const [submitted, setSubmitted] = useState(false);
 
-
-  })
-
-  return <h2>Hello</h2>;
+  return (
+    <div>
+      {page === 1 && (
+        <div>
+          <input
+            value={formData.name}
+            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+          ></input>
+          <input
+            type="email"
+            value={formData.email}
+            onChange={(e) =>
+              setFormData({ ...formData, email: e.target.value })
+            }
+          ></input>
+        </div>
+      )}
+      {page===2 && (
+        <div></div>
+      )}
+    </div>
+  );
 };
 
 export default Demo;
