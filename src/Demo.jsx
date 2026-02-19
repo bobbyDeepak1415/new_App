a2c29da07bc03567003d0dfa0d5944d38298fa02
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-02-19 15:22:52 +0530

    setup done

---
+++ src/Demo.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+const Demo = () => {
+
+    const [formData,setFormData]=useState({
+        name:"",
+        email:"",
+        role:"",
+        terms:""
+    })
+
+  return (
+    <div>
+      Hello
+    </div>
+  )
+}
+
+export default Demo
