366fa440cc7f238b4bb04d267da9aa382b28a8cf
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-02-19 17:22:23 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,81 +1,9 @@
 import React, { useState } from "react";
 
 const Demo = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    role: "",
-    terms: false,
-  });
-
-  const [page, setPage] = useState(1);
-
-  const [submitted, setSubmitted] = useState(false);
-
-  const isStep1Valid = formData.name && formData.email;
-  const isStep2Valid = formData.role && formData.terms;
-
-  if(submitted){
-    return (
-      <div>
-        <li>{formData.name}</li>
-        <li>{formData.email}</li>
-        <li>{formData.role}</li>
-        <span>agreed to terms:</span><li>{formData.terms}</li>
-      </div>
-    )
-  }
-
   return (
-    <div>
-      {page === 1 && (
-        <div>
-          <h2>Step 1</h2>
-          <input
-            placeholder="name..."
-            value={formData.name}
-            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-            name=""
-          ></input>
-          <input
-            type="email"
-            placeholder="email.."
-            value={formData.email}
-            onChange={(e) =>
-              setFormData({ ...formData, email: e.target.value })
-            }
-            name=""
-          ></input>
-          <button onClick={() => setPage(2)} disabled={!isStep1Valid}>
-            Next{" "}
-          </button>
-        </div>
-      )}
-      {page === 2 && (
-        <div>
-          <select
-            value={formData.role}
-            onChange={(e) => setFormData({ ...formData, role: e.target.value })}
-          >
-            <option>Select Your Role</option>
-            <option value="developer">Developer</option>
-            <option value="designer">designer</option>
-            <option value="manager">Manager</option>
-          </select>
-          <span>Agree terms and conditions:</span>
-          <input
-            type="checkbox"
-            value={formData.terms}
-            onChange={(e) =>
-              setFormData({ ...formData, terms: e.target.checked })
-            }
-          ></input>
-          <button onClick={()=>setPage(1)}>Back</button>
-          <button disabled={!isStep2Valid} onClick={()=>setSubmitted(true)}>Next</button>
-        </div>
-      )}
-    </div>
-  );
+    <h2>Hello</h2>
+  )
 };
 
 export default Demo;
