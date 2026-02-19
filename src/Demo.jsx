d792c5ae462d5e3f88aeec3a235442ffbaf97f1f
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-02-19 17:03:30 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,19 +1,50 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Demo = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    role: "",
+    terms: "",
+  });
 
-    const [formData,setFormData]=useState({
-        name:"",
-        email:"",
-        role:"",
-        terms:""
-    })
+  const [page, setPage] = useState(1);
+
+  const [submitted, setSubmitted] = useState(false);
+
+  const isStep1Valid = formData.name && formData.email;
+  const isStep2Valid = formData.role && formData.terms;
 
   return (
     <div>
-      Hello
+      {page === 1 && (
+        <div>
+          <h2>Step 1</h2>
+          <input
+            placeholder="name..."
+            value={formData.name}
+            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+            name=""
+          ></input>
+          <input
+            type="email"
+            placeholder="email.."
+            value={formData.email}
+            onChange={(e) =>
+              setFormData({ ...formData, email: e.target.value })
+            }
+            name=""
+          ></input>
+          <button
+            onClick={() => setPage(2)}
+            disabled={!isStep1Valid}
+          >
+            Next{" "}
+          </button>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Demo
+export default Demo;
