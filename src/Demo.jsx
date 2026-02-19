e0c01462414117799feee034c9056297bacb2287
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-02-20 00:15:33 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -3,6 +3,14 @@ import { useState } from "react";
 const Demo = () => {
 
   const [page,setPage]=useState(1)
+  const [formData,setFormData]=useState({
+    name:"",
+    email:"",
+    role:"",
+terms:false
+
+
+  })
 
   return <h2>Hello</h2>;
 };
