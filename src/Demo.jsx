89f3266cf35e56a3b87c1ba3f9bb6db7c0fcf15f
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-03-24 02:37:24 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
+import Child from "./Child";
 
 const Demo = () => {
+  const [count, setCount] = useState(0);
 
-  const [count,setCount]=useState(0)
+  return (
+    <div>
+      <h1>Hello from parent {count}</h1>
 
-  return <div>
-  <h1>Hello from parent</h1>
-
-  <button onClick={()=>setCount(count+1)}>click</button>
-  </div>
+      <button onClick={() => setCount(count + 1)}>click</button>
+      <Child/>
+    </div>
+  );
 };
 
 export default Demo;
