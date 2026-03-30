e6a2455b2f6d8d103e593eb48dee0ce050edd42c
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-03-31 03:30:16 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,18 +1,5 @@
-import React, { useState } from 'react'
-import Child from './Child'
-
 const Demo = () => {
+  return <div>Hello</div>;
+};
 
-
-  const [name,setName]=useState("Bobby")
-
-  return (
-    <div>
-      <Child setName={setName}/>
-    <h2>{name}</h2>
-      
-    </div>
-  )
-}
-
-export default Demo
+export default Demo;
