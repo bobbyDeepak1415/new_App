f958dbca70981455f75ee4bf609a2743b2e7a91f
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-03-31 03:26:27 +0530

    setup done

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,13 @@
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// // import './index.css'
-// // import App from "./App.jsx";
-// import Demo from "./Demo.jsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// import './index.css'
+// import App from "./App.jsx";
+import Demo from "./Demo.jsx";
 
-// createRoot(document.getElementById("root")).render(
-//   <StrictMode>
-//     {/* <App /> */}
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    {/* <App /> */}
 
-//     <Demo />
-//   </StrictMode>,
-// );
-
-function display(func) {
-  func();
-  func();
-}
-
-function printName() {
-  return "Bobby Deepak";
-}
-
-display(printName);
+    <Demo />
+  </StrictMode>,
+);
