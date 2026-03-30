bbaa0af9cf82307e965cfa12a98f4d42d73c6012
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-03-31 03:55:43 +0530

    setup done

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,13 @@
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// // import './index.css'
-// // import App from "./App.jsx";
-// import Demo from "./Demo.jsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// import './index.css'
+// import App from "./App.jsx";
+import Demo from "./Demo.jsx";
 
-// createRoot(document.getElementById("root")).render(
-//   <StrictMode>
-//     {/* <App /> */}
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    {/* <App /> */}
 
-//     <Demo />
-//   </StrictMode>,
-// );
-
-
-const nums=[1,2,3,4,5,6]
-
-
-console.log(nums.filter((num)=>num%2===0))
+    <Demo />
+  </StrictMode>,
+);
