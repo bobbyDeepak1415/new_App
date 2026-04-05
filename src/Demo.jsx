50a824704567b4e8496a3e0e66bd31ac5d716f28
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-04-05 18:28:28 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,5 +1,18 @@
+import { useState } from "react";
+
 const Demo = () => {
-  return <div>Hello</div>;
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <input value={count} onChange={(e) => setCount(e.target.value)} />
+      <button onClick={handleClick}>click</button>
+    </div>
+  );
 };
 
 export default Demo;
