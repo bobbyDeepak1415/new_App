86cdb3c23f1938d4c3881d9a938586c68bc7f48b
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-07-23 17:32:52 +0530

    setup done

--- src/SinglePageForm.jsx
+++ src/SinglePageForm.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 
 const SinglePageForm = () => {
-  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  const passwordRegex = /^(?=.*?[0-9])(?=.*?[A-Za-z]).{8,32}$/;
 
-  return <div></div>;
+  return <div>
+  </div>;
 };
 
 export default SinglePageForm;
