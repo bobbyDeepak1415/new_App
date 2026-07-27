7930487d1e1d95413ec0fc61885caf8de03c2fb4
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-07-27 16:45:58 +0530

    setup done

--- src/SinglePageForm.jsx
+++ src/SinglePageForm.jsx
@@ -63,7 +63,7 @@ const SinglePageForm = () => {
         <>
           <h2 style={successMsgStyle}>You Have successfully loggedIn</h2>
 
-          <pre>{JSON.stringify(formValues, undefined, 3)}</pre>
+          <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
         </>
       ) : (
         <form onSubmit={handleSubmit}>
