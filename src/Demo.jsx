e6f21c234223fbf2af7177943dce031cfe3889a5
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-07-27 16:24:01 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 
+//  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+//  const passwordRegex = /^(?=.*?[0-9])(?=.*?[A-Za-z]).{8,32}$/;
+
 const Demo = () => {
   return (
     <div>
--- src/SinglePageForm.jsx
+++ src/SinglePageForm.jsx
@@ -4,8 +4,6 @@ const SinglePageForm = () => {
   const initialValues = { username: "", email: "", password: "" };
 
   const [formValues, setFormValues] = useState(initialValues);
-  const [formErrors, setFormErrors] = useState({});
-  const [isSubmit, setIsSubmit] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -13,88 +11,46 @@ const SinglePageForm = () => {
     setFormValues({ ...formValues, [name]: value });
   };
 
-  const validate = (formValues) => {
-    const errors = {};
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    const passwordRegex = /^(?=.*?[0-9])(?=.*?[A-Za-z]).{8,32}$/;
-
-    if (!formValues.username) {
-      errors.username = "username required";
-    }
-    if (!formValues.email) {
-      errors.email = "email required";
-    } else if (!emailRegex.test(formValues.email)) {
-      errors.email = "Invalid email format";
-    }
-    if (!formValues.password) {
-      errors.password = "password required";
-    } else if (!passwordRegex.test(formValues.password)) {
-      errors.password = "Invalid password format";
-    }
-
-    return errors;
-  };
-
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    const errors = validate(formValues);
-
-    setFormErrors(errors);
-
-    if (Object.keys(errors).length === 0) {
-      setIsSubmit(true);
-    }
   };
 
   return (
-    <div className="form container">
-      <div>
-        {isSubmit ? (
-          <>
-            <h2>You have Logged in</h2>
-            {JSON.stringify(formValues, undefined, 2)}
-          </>
-        ) : (
-          <form onSubmit={handleSubmit}>
-            <h1>User Form</h1>
-            <div>
-              <label>Username:</label>
-              <input
-                value={formValues.username}
-                name="username"
-                type="text"
-                placeholder="username"
-                onChange={handleChange}
-              />
-            </div>
-            <p style={{ color: "red" }}>{formErrors.username}</p>
-            <div>
-              <label>Email:</label>
-              <input
-                value={formValues.email}
-                name="email"
-                type="email"
-                placeholder="email"
-                onChange={handleChange}
-              />
-            </div>
-            <p style={{ color: "red" }}>{formErrors.email}</p>
-            <div>
-              <label>Password:</label>
-              <input
-                value={formValues.password}
-                name="password"
-                type="password"
-                placeholder="password"
-                onChange={handleChange}
-              />
-            </div>
-            <p style={{ color: "red" }}>{formErrors.password}</p>
-            <button>Login</button>
-          </form>
-        )}
-      </div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h1>User Form:</h1>
+        <div>
+          <label htmlFor="form-username">Username:</label>
+          <input
+            value={formValues.username}
+            name="username"
+            placeholder="username"
+            onChange={handleChange}
+            id="form-username"
+          />
+        </div>
+        <div>
+          <label htmlFor="form-email">Email:</label>
+          <input
+            value={formValues.email}
+            name="email"
+            placeholder="email"
+            onChange={handleChange}
+            id="form-email"
+          />
+        </div>
+        <div>
+          <label htmlFor="form-username">Password:</label>
+          <input
+            value={formValues.password}
+            name="password"
+            placeholder="password"
+            onChange={handleChange}
+            id="form-password"
+          />
+        </div>
+        <button>Login</button>
+      </form>
     </div>
   );
 };
