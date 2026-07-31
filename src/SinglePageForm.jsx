9dceddad211df2fffad454e387135ecc9a70bc2d
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-07-31 19:50:51 +0530

    setup done

--- src/SinglePageForm.jsx
+++ src/SinglePageForm.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from "react";
 
 const SinglePageForm = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
 
-export default SinglePageForm
+  const handleCange=()=>{
+    
+  }
+
+  return (
+    <div
+      className="form-container"
+      style={{ height: "100vh", width: "100vw", backgroundColor: "gray" }}
+    >
+      <form>
+        <div className="form-field">
+          <label>Username:</label>
+          <input
+            style={{
+              background: "transparent",
+              border: "2px solid black",
+              margin: "20px",
+              onChange={handleChnage}
+            }}
+          />
+        </div>
+        <div className="form-field">
+          <label>Username:</label>
+          <input
+            style={{
+              background: "transparent",
+              border: "2px solid black",
+              margin: "20px",
+            }}
+          />
+        </div>
+        <div className="form-field">
+          <label>Username:</label>
+          <input
+            style={{
+              background: "transparent",
+              border: "2px solid black",
+              margin: "20px",
+            }}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SinglePageForm;
