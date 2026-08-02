8632dd6f6c48a27f4bee55800da9ff663763c139
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-02 21:11:41 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -5,14 +5,26 @@ const Demo = () => {
 
   const [page,SetPage]=useState(1)
 
-  const initialValues={name:"",email:"",role:"",terms:""}
+  const initialValues={username:"",email:"",role:"",terms:""}
+
+  const [formValues,setFromValues]=useState(initialValues)
 
 
 
   return (
     <div style={{height:"100vh",width:"100vw",backgroundColor:"gray"}}>
       
-<input></input>
+      <div className='form container'>
+        <div className='input field'>
+<label>Username:</label>
+<input name="username" value={formValues.username}/>
+        </div>
+        <div className='input field'>
+<label>email:</label>
+<input name="email" value={formValues.email}/>
+        </div>
+
+      </div>
 
     </div>
   )
