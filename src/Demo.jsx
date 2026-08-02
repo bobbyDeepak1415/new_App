208ed0d005c6aedd0a6fbdd5c17befacf13b9d53
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-02 21:52:36 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -3,10 +3,12 @@ import React, { useState } from "react";
 const Demo = () => {
   const [page, SetPage] = useState(1);
 
-  const initialValues = { username: "", email: "", role: "", terms: "" };
+  const initialValues = { username: "", email: "", role: "", terms: false };
 
   const [formValues, setFromValues] = useState(initialValues);
   const [error, setError] = useState("");
+
+  
 
   
 
@@ -43,36 +45,42 @@ const handleClick=()=>{
 
 return (
   <div style={{ height: "100vh", width: "100vw", backgroundColor: "gray" }}>
+    {page === 1 ? (
+      <div className="form container">
+        <div className="input field">
+          <label>Username:</label>
+          <input
+            type="text"
+            onChange={handleChange}
+            name="username"
+            value={formValues.username}
+          />
+        </div>
+        <div className="input field">
+          <label>email:</label>
+          <input
+            onChange={handleChange}
+            name="email"
+            value={formValues.email}
+          />
+        </div>
 
-{page===1? (
-<div className="form container">
-      <div className="input field">
-        <label>Username:</label>
-        <input
-          type="text"
-          onChange={handleChange}
-          name="username"
-          value={formValues.username}
-        />
+        {error}
       </div>
-      <div className="input field">
-        <label>email:</label>
-        <input
-          onChange={handleChange}
-          name="email"
-          value={formValues.email}
-        />
-      </div>
+    ) : (
+      <>
+        <select onChange={handleChange}>
+          <option value="">Select role</option>
+          <option value={formValues.role}>Developer</option>
+          <option value={formValues.role}>Designer</option>
+          <option value={formValues.role}>Manager</option>
+        </select>
 
-
-      {error}
-    </div>
-):(
-<p>Page2</p>
-)}
-<button onClick={handleClick}>Next</button>
-
-   
+        <span>You agree to terms</span>
+        <input type="checkbox" value={formValues.terms} onChange={handleChange} type="select" />
+      </>
+    )}
+    <button onClick={handleClick}>Next</button>
   </div>
 );
 };
