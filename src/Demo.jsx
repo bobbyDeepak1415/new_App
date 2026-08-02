d17d18b6039a4878b3942ebff4a5c4871e2a0bd1
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-02 21:05:57 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Demo = () => {
   return (
     <div>
-      
+      Hello
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 // import './index.css'
-import App from "./App.jsx";
-// import Demo from "./Demo.jsx";
+// import App from "./App.jsx";
+import Demo from "./Demo.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
 
-    {/* <Demo /> */}
+    <Demo />
   </StrictMode>,
 );
