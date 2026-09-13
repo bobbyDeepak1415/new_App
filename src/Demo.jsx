4ba85fb208883c9ab605c7d8876c5a7f38795470
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-09-13 13:22:51 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,25 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Demo = () => {
 
   const initialValues={userName:"",email:"",password:""}
 
-  
+  const [formValues,setFormValues]=useState(initialValues)
+
+  const [formErrors,setFormErrors]=useState({})
+
+  const [isSubmit,setIsSubmit]=useState(false)
+
+const successMsg={
+  color:"green",
+  fontSize:"20px",
+  fontWeight:"bold"
+}
 
   const formField={
     margin:"10px"
   }
 
+  const handleChange=()=>{
+
+  }
+
   return (
     <div>
-      <form>
+      {isSubmit ? (
+<p style={successMsg}>Successfully Logged In</p>
+      ):(
+<form>
         <div style={formField}>
           <label>Username:</label>
-          <input/>
+          <input value={formValues.userName} type='text' onChange={handleChange}/>
+        </div>
+        <div style={formField}>
+          <label>Email:</label>
+          <input value={formValues.email} type='email' onChange={handleChange}/>
+        </div>
+        <div style={formField}>
+          <label>Password:</label>
+          <input value={formValues.password} type='password' onChange={handleChange}/>
         </div>
 
-
+<button>Submit</button>
       </form>
+      )}
+      
     </div>
   )
 }
