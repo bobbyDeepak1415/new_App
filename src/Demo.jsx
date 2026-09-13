2a476853b1bf20a5bd8f856869c816412a1fe857
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-09-13 12:11:48 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,7 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Demo = () => {
-  return <div>Hello</div>;
+
+
+  const initialValues={name:"",password:"",email:""}
+
+  const [formValues,setFormValues]=useState(initialValues)
+  const [formErrors,setFormErrors]=useState({})
+
+  const [isSubmit,setIsSubmit]=useState(false)
+
+  const successMsg={
+    color:"green",
+    fontSize:"30px"
+  }
+  const formField={
+    margin:"20px"
+  }
+
+  const errorMsg={
+    color:"red",
+    fontSize:"20px"
+  }
+
+
+  const handleChange=()=>{
+
+    const {name,value}=e.target
+
+    setFormValues({...formValues,[name]:value})
+
+  }
+
+  const handleSubmit=(e)=>{
+e.preventDefault()
+
+
+  }
+
+  return <div>
+
+    {isSubmit ? (
+<p style={successMsg}>Successfully LoggedIn</p>
+    ):(
+<form onSubmit={handleSubmit}>
+      <div style={formField}>
+<label>Name:</label>
+      <input value={formValues.name} type="text" onChange={handleChange}/>
+      <p style={errorMsg}>{formErrors.name}</p>
+      </div>
+      <div style={formField}>
+<label>Email:</label>
+      <input value={formValues.email} type="email" onChange={handleChange}/>
+      <p style={errorMsg}>{formErrors.email}</p>
+      </div>
+      <div style={formField}>
+<label>Password:</label>
+      <input value={formValues.password} type="password" onChange={handleChange}/>
+      <p style={errorMsg}>{formErrors.password}</p>
+      </div>
+    </form>
+    )}
+    
+  </div>
 };
 
 export default Demo;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 // import './index.css'
-import App from "./App.jsx";
-// import Demo from "./Demo.jsx";
+// import App from "./App.jsx";
+import Demo from "./Demo.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
 
-    {/* <Demo /> */}
+    <Demo />
   </StrictMode>,
 );
 
