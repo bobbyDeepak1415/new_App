8e87cdc1bf7c1ca3d07d7fcc10f6a3646d69fca1
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-09-13 12:48:57 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
 const Demo = () => {
+
+  const initialValues={userName:"",email:"",password:""}
+
+  
+
+  const formField={
+    margin:"10px"
+  }
+
   return (
     <div>
-      
+      <form>
+        <div style={formField}>
+          <label>Username:</label>
+          <input/>
+        </div>
+
+
+      </form>
     </div>
   )
 }
